50e9548646042670339bfb6e32c9eb7c82dc9801
ariif <arf.pnp.amd@gmail.com>
2021-03-14 09:05:06 +0700

    commit

---
+++ app/src/main/java/com/hilt/newszilla/utils/AppConstants.kt
@@ -0,0 +1,24 @@
+package com.hilt.newszilla.utils
+
+object AppConstants {
+
+
+    const val IS_ONBOARDING_COMPLETED = "IS_ONBOARDING_COMPLETED"
+    const val NIGHT_MODE = "NIGHT_MODE"
+    const val SHARED_PREF = "SHARED_PREF"
+    @Volatile
+    var API_KEY = "Use your api key here"
+    const val ARG_CATEGORY = "category"
+    const val QUERY_PAGE_SIZE = 20
+    const val QUERY_HEADLINE_PAGE_SIZE = 10
+    const val SEARCH_PAGE_SIZE = 20
+    const val NEWS_SOURCE = "NEWS_SOURCE"
+    const val NEWS_COUNTRY = "NEWS_COUNTRY"
+
+    const val NEWS_LANGUAGE = "NEWS_LANGUAGE"
+    const val NEWS_SORTBY = "NEWS_SORT_BY"
+
+
+
+    const val SEARCH_NEWS_DELAY = 500L
+}
